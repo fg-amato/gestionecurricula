d2a6e13af1b2b3bcc6d11fcb71d039616dfff23d
flavio.g.amato <flavio.g.amato@gmail.com>
2022-02-24 22:41:04 +0100

    EsperienzaDAOImpl e CurriculumDAOImpl CRUD

--- src/it/gestionecurricula/dao/curriculum/CurriculumDAOImpl.java
+++ src/it/gestionecurricula/dao/curriculum/CurriculumDAOImpl.java
@@ -1,6 +1,10 @@
 package it.gestionecurricula.dao.curriculum;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import it.gestionecurricula.dao.AbstractMySQLDAO;
@@ -10,38 +14,184 @@ public class CurriculumDAOImpl extends AbstractMySQLDAO implements CurriculumDAO
 
 	@Override
 	public List<Curriculum> list() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		ArrayList<Curriculum> result = new ArrayList<Curriculum>();
+		Curriculum curriculumTemp = null;
+
+		try (Statement ps = connection.createStatement(); ResultSet rs = ps.executeQuery("select * from Curriculum")) {
+
+			while (rs.next()) {
+				curriculumTemp = new Curriculum();
+				curriculumTemp.setNome(rs.getString("NOME"));
+				curriculumTemp.setCognome(rs.getString("COGNOME"));
+				curriculumTemp.setTelefono(rs.getString("Telefono"));
+				curriculumTemp.setEmail(rs.getString("email"));
+				curriculumTemp.setDataDiNascita(rs.getDate("datadinascita"));
+				curriculumTemp.setId(rs.getLong("ID"));
+				result.add(curriculumTemp);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public Curriculum get(Long idInput) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (idInput == null || idInput < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		Curriculum result = null;
+		try (PreparedStatement ps = connection.prepareStatement("select * from Curriculum where id=?")) {
+
+			ps.setLong(1, idInput);
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					result = new Curriculum();
+					result.setNome(rs.getString("NOME"));
+					result.setCognome(rs.getString("COGNOME"));
+					result.setTelefono(rs.getString("Telefono"));
+					result.setEmail(rs.getString("email"));
+					result.setDataDiNascita(rs.getDate("datadinascita"));
+					result.setId(rs.getLong("ID"));
+				} else {
+					result = null;
+				}
+			} // niente catch qui
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int update(Curriculum input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement(
+				"UPDATE Curriculum SET nome=?, cognome=?, email=?, telefono=?, dataDiNascita=? where id=?;")) {
+			ps.setString(1, input.getNome());
+			ps.setString(2, input.getCognome());
+			ps.setString(3, input.getEmail());
+			ps.setString(4, input.getTelefono());
+			// quando si fa il setDate serve un tipo java.sql.Date
+			ps.setDate(5, new java.sql.Date(input.getDataDiNascita().getTime()));
+			ps.setLong(6, input.getId());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int insert(Curriculum input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement(
+				"INSERT INTO Curriculum (nome, cognome, email, telefono, datadinascita) VALUES (?, ?, ?, ?, ?);")) {
+			ps.setString(1, input.getNome());
+			ps.setString(2, input.getCognome());
+			ps.setString(3, input.getEmail());
+			ps.setString(4, input.getTelefono());
+			// quando si fa il setDate serve un tipo java.sql.Date
+			ps.setDate(5, new java.sql.Date(input.getDataDiNascita().getTime()));
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int delete(Curriculum input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement("DELETE FROM Curriculum WHERE ID=?")) {
+			ps.setLong(1, input.getId());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public List<Curriculum> findByExample(Curriculum input) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null)
+			throw new Exception("Valore di input non ammesso.");
+
+		ArrayList<Curriculum> result = new ArrayList<Curriculum>();
+		Curriculum curriculumTemp = null;
+
+		String query = "select * from user where 1=1 ";
+		if (input.getCognome() != null && !input.getCognome().isEmpty()) {
+			query += " and cognome like '" + input.getCognome() + "%' ";
+		}
+		if (input.getNome() != null && !input.getNome().isEmpty()) {
+			query += " and nome like '" + input.getNome() + "%' ";
+		}
+
+		if (input.getEmail() != null && !input.getEmail().isEmpty()) {
+			query += " and email like '" + input.getEmail() + "%' ";
+		}
+
+		if (input.getTelefono() != null && !input.getTelefono().isEmpty()) {
+			query += " and telefono like '" + input.getTelefono() + "%' ";
+		}
+
+		if (input.getDataDiNascita() != null) {
+			query += " and datadinascita='" + new java.sql.Date(input.getDataDiNascita().getTime()) + "' ";
+		}
+
+		try (Statement ps = connection.createStatement()) {
+			ResultSet rs = ps.executeQuery(query);
+
+			while (rs.next()) {
+				curriculumTemp = new Curriculum();
+				curriculumTemp.setNome(rs.getString("NOME"));
+				curriculumTemp.setCognome(rs.getString("COGNOME"));
+				curriculumTemp.setTelefono(rs.getString("Telefono"));
+				curriculumTemp.setEmail(rs.getString("email"));
+				curriculumTemp.setDataDiNascita(rs.getDate("datadinascita"));
+				curriculumTemp.setId(rs.getLong("ID"));
+				result.add(curriculumTemp);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
--- src/it/gestionecurricula/dao/esperienza/EsperienzaDAOImpl.java
+++ src/it/gestionecurricula/dao/esperienza/EsperienzaDAOImpl.java
@@ -1,6 +1,10 @@
 package it.gestionecurricula.dao.esperienza;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import it.gestionecurricula.dao.AbstractMySQLDAO;
@@ -10,38 +14,174 @@ public class EsperienzaDAOImpl extends AbstractMySQLDAO implements EsperienzaDAO
 
 	@Override
 	public List<Esperienza> list() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		ArrayList<Esperienza> result = new ArrayList<Esperienza>();
+		Esperienza esperienzaTemp = null;
+
+		try (Statement ps = connection.createStatement(); ResultSet rs = ps.executeQuery("select * from Esperienza")) {
+
+			while (rs.next()) {
+				esperienzaTemp = new Esperienza();
+				esperienzaTemp.setDescrizione(rs.getString("NOME"));
+				esperienzaTemp.setConoscenzeAcquisite(rs.getString("COGNOME"));
+				esperienzaTemp.setDataInizio(rs.getDate("DataInizio"));
+				esperienzaTemp.setDataFine(rs.getDate("datafine"));
+				esperienzaTemp.setId(rs.getLong("ID"));
+				result.add(esperienzaTemp);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public Esperienza get(Long idInput) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (idInput == null || idInput < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		Esperienza result = null;
+		try (PreparedStatement ps = connection.prepareStatement("select * from Esperienza where id=?")) {
+
+			ps.setLong(1, idInput);
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					result = new Esperienza();
+					result.setDescrizione(rs.getString("NOME"));
+					result.setConoscenzeAcquisite(rs.getString("COGNOME"));
+					result.setDataInizio(rs.getDate("DataInizio"));
+					result.setDataFine(rs.getDate("datafine"));
+					result.setId(rs.getLong("ID"));
+				} else {
+					result = null;
+				}
+			} // niente catch qui
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int update(Esperienza input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement(
+				"UPDATE Esperienza SET descrizione=?, conoscenzeAcquisite=?, dataInizio=?, datafine=? where id=?;")) {
+			ps.setString(1, input.getDescrizione());
+			ps.setString(2, input.getConoscenzeAcquisite());
+			ps.setDate(3, new java.sql.Date(input.getDataInizio().getTime()));
+			ps.setDate(4, new java.sql.Date(input.getDataFine().getTime()));
+			ps.setLong(5, input.getId());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int insert(Esperienza input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement(
+				"INSERT INTO Esperienza (descrizione, conoscenzeAcquisite, dataInizio, datafine, curriculum_id) VALUES (?, ?, ?, ?, ?);")) {
+			ps.setString(1, input.getDescrizione());
+			ps.setString(2, input.getConoscenzeAcquisite());
+			ps.setDate(3, new java.sql.Date(input.getDataInizio().getTime()));
+			ps.setDate(4, new java.sql.Date(input.getDataFine().getTime()));
+			ps.setLong(6, input.getCurriculum().getId());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int delete(Esperienza input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement("DELETE FROM Esperienza WHERE ID=?")) {
+			ps.setLong(1, input.getId());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public List<Esperienza> findByExample(Esperienza input) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (input == null)
+			throw new Exception("Valore di input non ammesso.");
+
+		ArrayList<Esperienza> result = new ArrayList<Esperienza>();
+		Esperienza esperienzaTemp = null;
+
+		String query = "select * from user where 1=1 ";
+		if (input.getConoscenzeAcquisite() != null && !input.getConoscenzeAcquisite().isEmpty()) {
+			query += " and ConoscenzeAcquisite like '" + input.getConoscenzeAcquisite() + "%' ";
+		}
+		if (input.getDescrizione() != null && !input.getDescrizione().isEmpty()) {
+			query += " and Descrizione like '" + input.getDescrizione() + "%' ";
+		}
+
+		if (input.getDataInizio() != null) {
+			query += " and DataInizio>='" + new java.sql.Date(input.getDataInizio().getTime()) + "' ";
+		}
+
+		if (input.getDataFine() != null) {
+			query += " and DataFine>='" + new java.sql.Date(input.getDataFine().getTime()) + "' ";
+		}
+
+		try (Statement ps = connection.createStatement()) {
+			ResultSet rs = ps.executeQuery(query);
+
+			while (rs.next()) {
+				esperienzaTemp = new Esperienza();
+				esperienzaTemp.setDescrizione(rs.getString("Descrizione"));
+				esperienzaTemp.setConoscenzeAcquisite(rs.getString("ConoscenzeAcquisite"));
+				esperienzaTemp.setDataInizio(rs.getDate("DataInizio"));
+				esperienzaTemp.setDataFine(rs.getDate("DataFine"));
+				esperienzaTemp.setId(rs.getLong("ID"));
+				result.add(esperienzaTemp);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
