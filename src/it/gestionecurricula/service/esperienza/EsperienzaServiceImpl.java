efe35a14c7f8bd6d459fa333f35d9c0bcabc0520
flavio.g.amato <flavio.g.amato@gmail.com>
2022-02-25 00:37:24 +0100

    Update EsperienzaServiceImpl

--- src/it/gestionecurricula/service/esperienza/EsperienzaServiceImpl.java
+++ src/it/gestionecurricula/service/esperienza/EsperienzaServiceImpl.java
@@ -59,8 +59,23 @@ public class EsperienzaServiceImpl implements EsperienzaService {
 
 	@Override
 	public int aggiorna(Esperienza input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			esperienzaDAO.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = esperienzaDAO.update(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
